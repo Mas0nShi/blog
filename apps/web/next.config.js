0ab7798af0cb81298e9b8412ef173fedde580807
Mas0nShi <fishilir@gmail.com>
2026-03-04 10:18:04 +0800

    refactor(web): update social media links and clean up configuration files

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,14 +1,7 @@
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
+// import path from 'node:path'
+// import { fileURLToPath } from 'node:url'
 
-import bundleAnalyzer from '@next/bundle-analyzer'
-
-const withBundleAnalyzer = bundleAnalyzer({
-  // eslint-disable-next-line no-process-env
-  enabled: process.env.ANALYZE === 'true'
-})
-
-export default withBundleAnalyzer({
+export default {
   staticPageGenerationTimeout: 300,
   images: {
     remotePatterns: [
@@ -17,27 +10,26 @@ export default withBundleAnalyzer({
       { protocol: 'https', hostname: 'images.unsplash.com' },
       { protocol: 'https', hostname: 'abs.twimg.com' },
       { protocol: 'https', hostname: 'pbs.twimg.com' },
-      { protocol: 'https', hostname: 's3.us-west-2.amazonaws.com' },
-      { protocol: 'https', hostname: 'avatars.githubusercontent.com' },
+      { protocol: 'https', hostname: 's3.us-west-2.amazonaws.com' }
     ],
     formats: ['image/avif', 'image/webp'],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
   },
 
-  webpack: (config) => {
-    // Workaround for ensuring that `react` and `react-dom` resolve correctly
-    // when using a locally-linked version of `react-notion-x`.
-    // @see https://github.com/vercel/next.js/issues/50391
-    const dirname = path.dirname(fileURLToPath(import.meta.url))
-    config.resolve.alias.react = path.resolve(dirname, '../../node_modules/react')
-    config.resolve.alias['react-dom'] = path.resolve(
-      dirname,
-      '../../node_modules/react-dom'
-    )
-    return config
-  },
+  // webpack: (config) => {
+  //   // Workaround for ensuring that `react` and `react-dom` resolve correctly
+  //   // when using a locally-linked version of `react-notion-x`.
+  //   // @see https://github.com/vercel/next.js/issues/50391
+  //   const dirname = path.dirname(fileURLToPath(import.meta.url))
+  //   config.resolve.alias.react = path.resolve(dirname, 'node_modules/react')
+  //   config.resolve.alias['react-dom'] = path.resolve(
+  //     dirname,
+  //     'node_modules/react-dom'
+  //   )
+  //   return config
+  // },
 
   // See https://react-tweet.vercel.app/next#troubleshooting
   transpilePackages: ['react-tweet']
-})
+}
